ecff3fbd4f5ac283eddf44952c0f24201ed1546a
roey <roeys1234@gmail.com>
2022-08-01 13:41:26 +0300

    init

--- client final project.py
+++ client final project.py
@@ -5,80 +5,71 @@ import tkinter.scrolledtext
 
 
 class Client:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.win = None
+        threading.Thread(target=self.gui_config).start()
 
-    def __init__(self, HOST, PORT):
-
-        self.HOST = HOST
-        self.PORT = PORT
         self.is_init_display = False
 
-        gui_thread = threading.Thread(target=self.gui_config, )
-        gui_thread.start()
-
-
+        self.overview = None
+        self.client = None
+        self.name = ""
+        self.message_box = None
 
     def connect(self):
-
         while not self.is_init_display:
             pass
 
-
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.connect((self.HOST, self.PORT))
-
-        rcv_thread = threading.Thread(target=self.recieve_msg, )
-        rcv_thread.start()
-
+        self.client.connect((self.host, self.port))
+        threading.Thread(target=self.receive_msg, ).start()
 
     def gui_config(self):
         self.name = str(input("enter your name "))
         self.win = tkinter.Tk()
-        #self.win.title(f"{name} chat")
-
         self.win.geometry("500x500")
-        self.my_label = tkinter.Label(self.win, text = 'chat:')
-        self.my_label.place(x = 230, y = 1)
+        top_title = tkinter.Label(self.win, text='chat:')
+        top_title.place(x=230, y=1)
+        
+        self.overview = tkinter.scrolledtext.ScrolledText(self.win, height=20, width=55)
+        self.overview.place(relx=0.05, y=20)
+        self.overview.config(state="disabled")
 
-        self.my_text1 = tkinter.scrolledtext.ScrolledText(self.win, height= 20, width = 55)
-        self.my_text1.place(relx = 0.05, y = 20)
-        self.my_text1.config(state="disable")
+        message_title = tkinter.Label(self.win, text="message:")
+        message_title.place(x=220, y=375)
 
-        self.my_label2 = tkinter.Label(self.win, text = "massege:")
-        self.my_label2.place(x = 220, y = 375)
+        self.message_box = tkinter.Text(self.win, height=2, width=55)
+        self.message_box.place(relx=0.05, y=400)
 
-        self.my_text2 = tkinter.Text(self.win, height=2, width=55)
-        self.my_text2.place(relx = 0.05, y = 400)
-
-
-        self.my_btn = tkinter.Button(self.win, text = "send massege:", command = self.button1)
-        self.my_btn.place(x = 200, y = 450)
+        my_btn = tkinter.Button(self.win, text="send message:", command=self.button1)
+        my_btn.place(x=200, y=450)
 
         self.is_init_display = True
         self.win.mainloop()
 
     def button1(self):
-        self.my_text1.config(state="normal")
-        self.message = f"{self.name}: {self.my_text2.get('1.0', 'end')}"
-        self.client.send(self.message.encode("utf-8"))
-        self.recieve_msg_from_server = self.client.recv(1024).decode("utf-8")
-        self.my_text1.insert("0.0",self.recieve_msg_from_server)
-        self.my_text1.config(state="disable")
+        self.overview.config(state="normal")
+        message = f"{self.name}: {self.message_box.get('1.0', 'end')}"
+        self.client.sendall(message.encode("utf-8"))
+        self.overview.insert("0.0", f'{self.client.recv(1024).decode("utf-8")}')
+        self.overview.config(state="disabled")
 
-    def recieve_msg(self):
-            index = 0
-            while index < 100:
-                try:
-                    message = self.client.recv(1024).decode('utf-8')
-                    print(message)
-                    self.my_text1.config(state="normal")
-                    self.my_text1.insert("end","\n" +  str(message) )
-                    self.my_text1.yview("end")
-                    self.my_text1.config(state="disable")
-                    index += 1
-                    self.client.send("Accept Message".encode('utf-8'))
-                except Exception as err:
-                    print("Error" + str(err))
+    def receive_msg(self):
+        index = 0
+        while index < 100:
+            try:
+                message = self.client.recv(1024).decode('utf-8')
+                self.overview.config(state="normal")
+                self.overview.insert("end", "\n" + str(message))
+                self.overview.yview("end")
+                self.overview.config(state="disable")
+                index += 1
+                self.client.send("Accept Message".encode('utf-8'))
+            except Exception as err:
+                print("Error" + str(err))
 
 
-a = Client(socket.gethostbyname(socket.gethostname()), 9090)
+a = Client("192.168.1.62", 9090)
 a.connect()
--- server final project.py
+++ server final project.py
@@ -7,19 +7,21 @@ HOST = socket.gethostbyname(socket.gethostname())
 PORT = 9090
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
-server.listen()
 
 
-def recieve_messages():
+def receive_messages():
     while True:
+        server.listen()
         client_socket, address = server.accept()
         # client_socket.send(f"the code is: {code}".encode("utf-8"))
         print(f"connected to: {address}")
         addresses_list.append(address)
         my_message = client_socket.recv(1024).decode("utf-8")
         history_msg = open("history_messages.txt", "w")
-        history_msg.write(f"{my_message}\n")
+        history_msg.write(f"{my_message}")
+        history_msg.close()
         client_socket.send(f"{my_message}".encode("utf-8"))
+        client_socket.close()
 
 
-recieve_messages()
+receive_messages()
