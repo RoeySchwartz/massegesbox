ecacfbfb5a966b0f3a77204633963c2a8a30152f
Roey Schwartz <roeys1234@gmail.com>
2022-08-10 13:46:56 +0300

    init

--- server final project.py
+++ server final project.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import time
 
 addresses_list = []
 
@@ -7,21 +8,21 @@ HOST = socket.gethostbyname(socket.gethostname())
 PORT = 9090
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
-threading.Thread(target=server.listen).start()
 
 
-def receive_messages():
+def receive_messages_from_client():
     while True:
+        threading.Thread(target=server.listen()).start()
         client_socket, address = server.accept()
-        # client_socket.send(f"the code is: {code}".encode("utf-8"))
         print(f"connected to: {address}")
         addresses_list.append(address)
-        my_message = client_socket.recv(1024).decode("utf-8")
+        message_from_server = client_socket.recv(1024).decode("utf-8")
         history_msg = open("studentsgrades.txt", "r+")
-        history_msg.write(f"{my_message}")
+        history_msg.write(f"{message_from_server}")
         history_msg.close()
-        client_socket.send(f"{my_message}".encode("utf-8"))
-        server.shutdown(1)
+        client_socket.send(f"{message_from_server}".encode("utf-8"))
+        server.shutdown(2)
+        time.sleep(2.0)
 
 
-receive_messages()
+receive_messages_from_client()
