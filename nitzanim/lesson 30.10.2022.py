32e45b4f902dbd6d1c7f33ddf44cda6e32a27441
roeys <roeys1234@gmail.com>
2022-10-30 19:36:21 +0200

    init

---
+++ nitzanim/lesson 30.10.2022.py
@@ -0,0 +1,42 @@
+# #q6
+# number1 = int(input("enter a number "))
+# number2 = int(input("enter a number "))
+# summer = number1+ number2
+#
+# print(summer)
+# print(summer%5)
+
+# q9
+# number_of_checks = int(input("enter the number of the checks "))
+# number_of_the_positive = int(input("enter the number of the positive checks "))
+#
+# print((number_of_the_positive*100)/number_of_checks)
+
+# q15
+# number_of_the_people = int(input("enter the number of the people "))
+# print(f"you need to choose {number_of_the_people//3} tents")
+
+# q7
+# number_of_the_works_hours = int(input("enter the number of your hours work "))
+# salary_for_an_hour = int(input("enter your salary "))
+# salary_for_a_month = salary_for_an_hour * number_of_the_works_hours
+#
+# income_tax = salary_for_a_month * 0.1
+# socialy_security_tax = salary_for_a_month * 0.05
+# health_tax = salary_for_a_month * 0.1
+#
+# real_salary = salary_for_a_month - income_tax - socialy_security_tax - health_tax
+#
+# print(f"your salary is {real_salary}")
+
+# q8
+# celsius = int(input("enter the degrees in celsius "))
+# farenhit = ((celsius * 9) / 5) + 32
+#
+# print(f"the degrees in farenhit is {farenhit} degrees")
+
+# challenge1:
+# x = int(input("enter a number "))
+# x += 1
+#
+# print(x * x * 50)
