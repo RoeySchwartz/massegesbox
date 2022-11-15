07e3bdf64db5089670e613c76e2d3d846e1195c3
roeys <roeys1234@gmail.com>
2022-11-15 18:10:31 +0200

    init

---
+++ nitzanim/lessons/lesson 15.11.2022.py
@@ -0,0 +1,22 @@
+# q1 board:
+# pet = str(input("enter an pet"))
+#
+# if pet == "dog":
+#     print("hoof")
+# elif pet == "cat":
+#     print("meow")
+# else:
+#     print("this pet is not in the list")
+
+# q2 board:
+# num1 = int(input("enter a number: "))
+#
+# match num1:
+#     case 1:
+#         print(num1)
+#     case 2:
+#         num2 = int(input("enter a number: "))
+#         print(num1 + num2)
+#     case 3:
+#         print("good job")
+
