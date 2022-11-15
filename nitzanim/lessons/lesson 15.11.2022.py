ee9be2f630d14ac04d9f3d8801bd9b28e1ae5dea
roeys <roeys1234@gmail.com>
2022-11-15 19:44:26 +0200

    init

--- nitzanim/lessons/lesson 15.11.2022.py
+++ nitzanim/lessons/lesson 15.11.2022.py
@@ -20,3 +20,46 @@
 #     case 3:
 #         print("good job")
 
+# q3 board:
+# holiday = bool(input("is there a holiday today? "))
+# day = int(input("what's the day today? "))
+#
+# if holiday:
+#     if day % 2 == 0:
+#         print("today benji will go to the beach")
+#     else:
+#         print("today benji will go to play football")
+# else:
+#     print("today benji will sleep")
+
+# q4 board:
+# age = int(input("enter your age: "))
+#
+# if 0 < age <= 120:
+#     if age >= 18:
+#         print("you are an adult ")
+#     else:
+#         print("you are not an adult ")
+# else:
+#     print("it is impossible")
+
+# q5 board:
+# name = input("Enter your name: ")
+# name_len = len(name)
+# if name_len == 5:
+#     print("your name is 5 letters long, Hamsa Hamsa Hamsa!")
+# elif name_len > 5:
+#     if name_len > 10:
+#         if name_len > 15:
+#             print("Are you serious? did you invent this name???")
+#         else:
+#             print("You have a very long name dude!")
+#     else:
+#         print("You have a long but normal name length, that's nice and original")
+# else:
+#     if name_len < 2:
+#         print("That's not a name it's a letter only")
+#     elif name_len < 3:
+#         print("You have a two letters pretty name")
+#     else:
+#         print("You have a normal length name!")
