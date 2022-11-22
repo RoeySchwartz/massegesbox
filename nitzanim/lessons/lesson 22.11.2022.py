5bc50657ed20897b7083728cfb8ad468ef3f8643
roeys <roeys1234@gmail.com>
2022-11-22 17:39:52 +0200

    init

---
+++ nitzanim/lessons/lesson 22.11.2022.py
@@ -0,0 +1,22 @@
+# ex_1:
+# EVEN = "Even"
+# ODD = "Odd"
+# number = 9
+# if number % 2 == 0:
+#     print(9, "is", EVEN)
+# else:
+#     print(9, "is", ODD)
+# number = 8
+# if number % 2 == 0:
+#     print(8, "is", EVEN)
+# else:
+#     print(8, "is", ODD)
+
+# # ex_2:
+# length = int(input("Enter length: "))
+# width = int(input("Enter width: "))
+# total_fence = 2 * length + 2 * width
+# if total_fence < 120:
+#     print("Request received")
+# else:
+#     print("Choose shorter fence")
