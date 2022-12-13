9d7d93b22ac806882a07bf9cc510eaa2dbb3897b
roeys <roeys1234@gmail.com>
2022-12-13 17:45:39 +0200

    init

---
+++ nitzanim/lessons/lesson 13.12.2022.py
@@ -0,0 +1,33 @@
+# ex-1:
+# counter = 0
+# for number in range(1, 9):
+#     counter += number
+# print(counter)
+
+# ex-2:
+# for number in range(2, 10, 2):
+#     print(number)
+
+# ex-3:
+# string = "hello"
+# for letter in string:
+#     print(letter, end=",")
+
+# ex-4:
+# string = "Nitzanim rules!"
+# counter_of_i = 0
+# for letter in string:
+#     if letter == "i":
+#         counter_of_i += 1
+# print(counter_of_i)
+
+# ex-5:
+# counter = 0
+# for number in range(5):
+#     num = int(input("enter a number: "))
+#     if num < 0:
+#         break
+#     counter += num
+# print(counter)
+
+# ex-6:
